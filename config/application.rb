65c227ddf55906582059ec8629e34d9a06c122cf
Marcin Felczak <feleck@gmail.com>
2018-07-10 19:30:28 +0200

    Modify generators to skip tests, assets and helpers and add haml template engine

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.template_engine :haml
+      generate.test_framework false
+    end
   end
 end
