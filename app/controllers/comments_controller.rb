dd55f746c1915650ce6408872120efcf3eb691e1
Marcin Felczak <feleck@gmail.com>
2018-07-25 12:49:45 +0200

    Add error handling on saving comments and merge user to permitted params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,16 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-    @comment.user = current_user
-    @comment.save
-    redirect_to post_path(@post)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      redirect_to @post, flash: { error: "Can't add empty comment!" }
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
   end
 end
