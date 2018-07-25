69482dea4e7fa08098febffe864f1345ba9d6a5c
Marcin Felczak <feleck@gmail.com>
2018-07-25 10:17:33 +0200

    Add index to users in comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_095526) do
+ActiveRecord::Schema.define(version: 2018_07_24_120614) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_07_17_095526) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
